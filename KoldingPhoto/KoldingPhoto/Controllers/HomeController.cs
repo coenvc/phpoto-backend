b8f85050cb4b315590c03620e951b8b749e07eea
Dennis Vermeulen <d.vermeulen@me.com>
2017-09-28 11:36:05 +0200

    Addes pages

--- KoldingPhoto/KoldingPhoto/Controllers/HomeController.cs
+++ KoldingPhoto/KoldingPhoto/Controllers/HomeController.cs
@@ -8,23 +8,35 @@ namespace KoldingPhoto.Controllers
 {
     public class HomeController : Controller
     {
+
+        //private User model = new User();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+        //public ActionResult Index(string username, string password)
+        //{
+        //    if (model.LogGebruikerIn(username, password) != null)
+        //    {
+        //        Session["username"] = username;
+        //        Session.Timeout = 30;
+        //        return RedirectToAction();
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+        //} 
+
+        public ActionResult Timeline()
+        {
+            return View("Timeline");
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+
     }
 }
